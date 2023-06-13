b57521a6d497720a6809e455b5f498e6af78fb9a
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-06-13 01:24:10 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const targetNum = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === targetNum) return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -7,7 +14,12 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+  I need a function that checks the array, adds the different numbers together,
+  then returns true if 2 numbers in the array equal the target number,
+  and returns false if there are no numbers that would equal the target number
+  in sum.
+
+  
 */
 
 /*
@@ -17,6 +29,11 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 69, 7, 21, 3], 90));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
